7fc84b3735632a1ea4d33af2e4bcfbef0dcf6569
Uriah Harston <uharston@gmail.com>
2020-05-06 12:35:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,17 +21,41 @@ def assign_rooms(array)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees)
-   x = assign_rooms(attendees).length
-   n = assign_rooms(attendees)
-   y = 0
-   while y < x do
-   puts "#{n[y]}"
-   y += 1
-  #  x.each do
-  #
-  #
-  # attendees.each.with_index(1) do |speaker, index|
-  #   puts "Hello, #{speaker}! You'll be assigned to room #{index}!"
+   x = batch_badge_creator(attendees)
+   x.each do |speaker| 
+     puts "#{speaker}"
+   end
+   y = assign_rooms(attendees)
+   y.each do |room_assignments|
+     puts "#{room_assignments}"
+   end
  end
-end
+ 
+#    x = assign_rooms(attendees).length
+#    n = assign_rooms(attendees)
+#    y = 0
+#    while y < x do
+#    puts "#{n[y]}"
+#    y += 1
+#   #  x.each do
+#   #
+#   #
+#   # attendees.each.with_index(1) do |speaker, index|
+#   #   puts "Hello, #{speaker}! You'll be assigned to room #{index}!"
+#  end
+# end
+# basket = ["apple 1","apple 2","apple 3","apple 4","apple 5","apple 6","apple 7","apple 8","apple 9","apple 10"]
+#  
+# apples_in_basket = basket.size # Step 1
+# apples_taken_out = 0 # Step 2
+#  
+# loop do # Step 3
+#     if apples_taken_out < apples_in_basket 
+#         # Step 4
+#         puts "Taking out #{basket[apples_taken_out]}"
+#         apples_taken_out += 1
+#     else
+#         # Step 5
+#         break
+#     end
+# end
