dc1cfa77f1d22f542d2a7c868a22d5073ebed7a5
Uriah Harston <uharston@gmail.com>
2020-05-06 12:35:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,7 +22,7 @@ end
 
 def printer(attendees)
    x = batch_badge_creator(attendees)
-   x.each do |speaker| 
+   x.each do |speaker|
      puts "#{speaker}"
    end
    y = assign_rooms(attendees)
@@ -30,7 +30,7 @@ def printer(attendees)
      puts "#{room_assignments}"
    end
  end
- 
+
 #    x = assign_rooms(attendees).length
 #    n = assign_rooms(attendees)
 #    y = 0
@@ -45,12 +45,12 @@ def printer(attendees)
 #  end
 # end
 # basket = ["apple 1","apple 2","apple 3","apple 4","apple 5","apple 6","apple 7","apple 8","apple 9","apple 10"]
-#  
+#
 # apples_in_basket = basket.size # Step 1
 # apples_taken_out = 0 # Step 2
-#  
+#
 # loop do # Step 3
-#     if apples_taken_out < apples_in_basket 
+#     if apples_taken_out < apples_in_basket
 #         # Step 4
 #         puts "Taking out #{basket[apples_taken_out]}"
 #         apples_taken_out += 1
